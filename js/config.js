68432066eefea790db25aa06bef216519eb8b7b9
P-MH <miraculum95@gmail.com>
2023-07-19 16:05:43 +0900

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-2_2XdmEIhHO', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3dfvt1jqr6usnm096g2l4vsunu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
